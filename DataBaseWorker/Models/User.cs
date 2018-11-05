b8886c3d0f73c6db240b5a4a031d91b769f8bcf4
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-04 20:48:17 -0800

    [PATH] Add Role for User, Clear unused libraries and rebase structure

--- DataBaseWorker/Models/User.cs
+++ DataBaseWorker/Models/User.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataBaseWorker.TypeRole;
+using System;
 
-namespace DataBaseWorker.Models
+namespace DataBase.Models
 {
     class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int CooperatorId { get; set; }
+        public Role Role { get; set; }
+        public DateTime LastEnter { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
     }
 }
--- DataBaseWorker/Models/WorkBook.cs
+++ DataBaseWorker/Models/WorkBook.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DBAgregator.Models
+namespace DataBase.Models
 {
     class WorkBook
     {
@@ -14,5 +10,6 @@ namespace DBAgregator.Models
         public int OrderNumber { get; set; }
         public DateTime Date { get; set; }
         public string Reason { get; set; }
+        public DateTime Date { get; set; }
     }
 }
