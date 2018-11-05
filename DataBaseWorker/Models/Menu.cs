cfedd7ca6a030d507ec4d7623352daf1301878cb
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-04 20:35:41 -0800

    [ADD] Models need for our solution

---
+++ DataBaseWorker/Models/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAgregator.Models
+{
+    class Menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int CooperatorId { get; set; }
+    }
+}
---
+++ DataBaseWorker/Models/WorkBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAgregator.Models
+{
+    class WorkBook
+    {
+        public int Id { get; set; }
+        public int CooperatorId { get; set; }
+        public int PositionId { get; set; }
+        public int OrderNumber { get; set; }
+        public DateTime Date { get; set; }
+        public string Reason { get; set; }
+    }
+}
