9cd27bb6ce0ce9b88d91918455dcbc1a50214b4a
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 06:58:55 -0800

    [ADD|DataBaseLIB] Add objects for send to clients

---
+++ DatabaseMiddlware/ObjectsAfterDb/Dish.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DatabaseMiddlware.ObjectsAfterDb
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public string [] Products { get; set; }
+        public double Outer { get; set; }
+    }
+}
