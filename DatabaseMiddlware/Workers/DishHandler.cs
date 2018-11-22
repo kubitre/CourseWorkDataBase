bea1108a8fbe41f5587abad24fcefe94c17d3fe6
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 06:56:59 -0800

    [ADD|DataBaseLIB] Add workers for user handler and DishHandler

---
+++ DatabaseMiddlware/Workers/DishHandler.cs
@@ -0,0 +1,73 @@
+﻿using DatabaseMiddlware.ObjectsAfterDb;
+using System;
+using System.Linq;
+
+namespace DatabaseMiddlware.Workers.Dishes
+{
+    public static class DishHandler
+    {
+        public static void AddDish(string name, Guid cooperatorId)
+        {
+            using(var context = new Context())
+            {
+                var cooperator = context.Cooperators.Where(x => x.Id == cooperatorId).ToArray()[0];
+
+                if(cooperator != null)
+                {
+                    context.Dishes.Add(new Models.Dish { Name = name, Cooperator = cooperator});
+                    context.SaveChanges();
+                }
+                else
+                    throw new ArgumentException("Cooperator was not found!");
+            }
+        }
+
+        public static Dish GetDish(Guid id)
+        {
+            using(var context = new Context())
+            {
+                var dish = context.Dishes.Where(x => x.Id == id).ToArray().FirstOrDefault();
+                if (dish == null)
+                    throw new ArgumentException("Dish does not exist!");
+
+                var recipe = context.Recipes.Where(x => x.Dish.Id == id).ToList();
+                if (recipe == null)
+                    throw new ArgumentException("Recpe does not exist!");
+
+
+
+                var productsInDish = from recipeProduct in context.Recipes
+                                     join product in context.Products on recipeProduct.Product.Id equals product.Id
+                                     select new { Name = product.Name, Amount = recipeProduct.Amount, Price = product.Price };
+
+                Console.WriteLine($"Dish [{dish.Id};{dish.Name}]:");
+
+
+                var outer = 0.0;
+
+
+                foreach(var element in productsInDish.ToList())
+                {
+                    outer += element.Price * element.Amount;
+                    Console.WriteLine($"Product [{element.Name}] : {element.Amount} x {element.Price} = {element.Price * element.Amount}");
+                }
+
+                Console.WriteLine($"Summary: {outer}");
+
+                return new Dish();
+
+            }
+        }
+        
+
+        public static void RemoveDish(string name)
+        {
+
+        }
+
+        public static void RemoveDish(Guid id)
+        {
+
+        }
+    }
+}
---
+++ DatabaseMiddlware/Workers/UserHandler.cs
@@ -0,0 +1,52 @@
+﻿namespace DatabaseMiddlware.Workers.Users
+{
+    using System;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using DatabaseMiddlware.Models;
+    using DatabaseMiddlware.TypeRole;
+
+    public static class UserHandler
+    {
+        public static void CreateNewUser(string username, string password, int roleId, User creator)
+        {
+            if (creator.Role.Equals(Role.Type.Administrator) | creator.Role.Equals(Role.Type.Programmer))
+            {
+                var newUser = new User { Name = username, Password = GetHashSha(password), Role = roleId, LastUpdated = DateTime.Now };
+            }
+            else if (creator.Role.Equals(Role.Type.Calculator))
+                throw new Exception("Calculator was not access for create new users!");
+            else
+                throw new Exception("Nobody was create new user!");
+        }
+
+        public static bool Authentificate(string username, string password)
+        {
+            using (var context = new Context())
+            {
+                var userFromDb = context.Users.Where(x => x.Name.Equals(username) & x.Password.Equals(GetHashSha(password))).FirstOrDefault();
+
+                if(userFromDb == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public static string GetHashSha(string str)
+        {
+            using (var sha512Hash = SHA512.Create())
+            {
+                var fromStrToBytes = Encoding.UTF8.GetBytes(str);
+                var hashBytes = sha512Hash.ComputeHash(fromStrToBytes);
+                string hash = Encoding.UTF8.GetString(hashBytes);
+                return hash;
+            }
+        }
+    }
+}
