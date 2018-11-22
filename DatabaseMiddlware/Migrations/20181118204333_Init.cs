0c5adeab9aed6eddd2d47bf461bef94edc4f16fd
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 06:57:25 -0800

    [ADD|DataBaseLIB] Add stable migrations

---
+++ DatabaseMiddlware/Migrations/20181118204333_Init.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseMiddlware.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Streets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Streets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cooperators",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    LastName = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    PositionId = table.Column<Guid>(nullable: true),
+                    CategoryId = table.Column<Guid>(nullable: true),
+                    StreetId = table.Column<Guid>(nullable: true),
+                    Building = table.Column<int>(nullable: false),
+                    Flat = table.Column<int>(nullable: false),
+                    BirthDay = table.Column<DateTime>(nullable: false),
+                    Salary = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cooperators", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cooperators_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cooperators_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cooperators_Streets_StreetId",
+                        column: x => x.StreetId,
+                        principalTable: "Streets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CooperatorId = table.Column<Guid>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dishes_Cooperators_CooperatorId",
+                        column: x => x.CooperatorId,
+                        principalTable: "Cooperators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    CooperatorId = table.Column<Guid>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Menus_Cooperators_CooperatorId",
+                        column: x => x.CooperatorId,
+                        principalTable: "Cooperators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    CooperatorId = table.Column<Guid>(nullable: true),
+                    Role = table.Column<int>(nullable: false),
+                    LastEnter = table.Column<DateTime>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    LastUpdated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Cooperators_CooperatorId",
+                        column: x => x.CooperatorId,
+                        principalTable: "Cooperators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkBooks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CooperatorId = table.Column<Guid>(nullable: true),
+                    PositionId = table.Column<Guid>(nullable: true),
+                    OrderNumber = table.Column<int>(nullable: false),
+                    Reason = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkBooks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkBooks_Cooperators_CooperatorId",
+                        column: x => x.CooperatorId,
+                        principalTable: "Cooperators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_WorkBooks_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DishId = table.Column<Guid>(nullable: true),
+                    ProductId = table.Column<Guid>(nullable: true),
+                    Amount = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Recipes_Dishes_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Recipes_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MenuItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    MenuId = table.Column<Guid>(nullable: true),
+                    Outer = table.Column<double>(nullable: false),
+                    DishId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MenuItems_Dishes_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MenuItems_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cooperators_CategoryId",
+                table: "Cooperators",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cooperators_PositionId",
+                table: "Cooperators",
+                column: "PositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cooperators_StreetId",
+                table: "Cooperators",
+                column: "StreetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_CooperatorId",
+                table: "Dishes",
+                column: "CooperatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItems_DishId",
+                table: "MenuItems",
+                column: "DishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItems_MenuId",
+                table: "MenuItems",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menus_CooperatorId",
+                table: "Menus",
+                column: "CooperatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_DishId",
+                table: "Recipes",
+                column: "DishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_ProductId",
+                table: "Recipes",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CooperatorId",
+                table: "Users",
+                column: "CooperatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkBooks_CooperatorId",
+                table: "WorkBooks",
+                column: "CooperatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkBooks_PositionId",
+                table: "WorkBooks",
+                column: "PositionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuItems");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "WorkBooks");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Cooperators");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+
+            migrationBuilder.DropTable(
+                name: "Streets");
+        }
+    }
+}
