8e0fea8d2a528f2cb817001411f312947b90ff48
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 06:57:57 -0800

    [ADD|DataaBaseLIB] Add roles for users

---
+++ DatabaseMiddlware/Additional/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace DatabaseMiddlware.TypeRole
+{
+    public static class Role
+    {
+        public enum Type { 
+            Administrator = 0,
+            Programmer = 1,
+            Calculator = 2
+        }
+    }
+}
