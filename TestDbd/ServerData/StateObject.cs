76578b4c279f1e6e4e057d8038f5f47d9d0244a3
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 17:48:33 -0800

    [ADD|Server] Add project and State for our server socket

---
+++ TestDbd/ServerData/StateObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ServerDB.ServerData
+{
+    public class StateObject
+    {
+        public Socket WorkSocket = null;
+        public const int BufferSize = 1024;
+        public byte[] Buffer = new byte[BufferSize];
+        public StringBuilder MessageForReciveData = new StringBuilder();
+    }
+}
