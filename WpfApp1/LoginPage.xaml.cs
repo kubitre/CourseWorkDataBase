fc52aa466a0921fa47d33f24a6762189a6c14832
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 17:57:03 -0800

    [ADD|AdminPanel] Create window for login user in our system

---
+++ WpfApp1/LoginPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using AdminPanel.Tcp;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace AdminPanel
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : MetroWindow
+    {
+
+
+        public LoginPage()
+        {
+
+            InitializeComponent();
+            //Client.NewMess += GetMessageFromServer;
+            //this.Dispatcher.Invoke(new ThreadStart(() => Client.Run()));
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (CheckData())
+                {
+                    var mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    var errorPanel = new ErrorConnect();
+                    errorPanel.Show();
+                    this.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                GetMessage(ex.Message);
+            }
+            
+        }
+
+        private bool CheckData()
+        {
+            try
+            {
+                if (ValidatorsAndCheckers.Validation.Validate(0, this.LoginUsernameTextBox.Text) & ValidatorsAndCheckers.Validation.Validate(1, this.LoginPasswordTextBox.Password))
+                {
+                    if (Client.SendAuthentification(this.LoginUsernameTextBox.Text, this.LoginPasswordTextBox.Password))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    GetMessageAuth("Введёные вами данные неверные, либо такой пользователь не существует в системе! Обратитесь к администратору или программисту!");
+                    Thread.Sleep(5000);
+                    return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private async void GetMessage(string mess) => await this.ShowMessageAsync("Ошибка!", "Пожалуйста, проверьте вводимые данные, т.к. мы обнаружили, что " + mess);
+
+        private async void GetMessageAuth(string mess) => await this.ShowMessageAsync("Ошибка входа!", mess);
+
+        
+
+    }
+}
