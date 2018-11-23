bf3e2a552936d680047d09d8526bb2721cf7588f
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 17:51:42 -0800

    [ADD|Server] Add run for server worker

---
+++ TestDbd/Program.cs
@@ -0,0 +1,15 @@
+﻿using DatabaseMiddlware;
+
+namespace ServerDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DatabaseMiddlware.Class1.CreateTestUser();
+            var server = new Server();
+            server.Run();
+        }
+    }
+}
