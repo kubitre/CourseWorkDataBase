e29f6dc4f3900ca4a2e5e01485b5a1fb8aad9d4e
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 18:05:14 -0800

    [ADD|AdminPanel|NetworkMiddleWare] Create Entrypoint for client listener, when send request to server for aggregate datas and other actions

---
+++ WpfApp1/NetworkMiddlware/Client.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdminPanel.NetworkMiddlware
+{
+    public static class Client
+    {
+        private static int _port = 65432;
+        private static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), _port);
+
+        //Signals for thread worker
+        internal static ManualResetEvent connectDone =
+        new ManualResetEvent(false);
+        internal static ManualResetEvent sendDone =
+            new ManualResetEvent(false);
+        internal static ManualResetEvent receiveDone =
+            new ManualResetEvent(false);
+
+        private static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public delegate void Attention(string mess);
+        public static event Attention NewMess;
+
+        public static void Run()
+        {
+            var worker = new Thread(new ThreadStart(Worker));
+            worker.Start();
+        }
+
+        //public static bool TestConnection()
+        //{
+        //    try
+
+        //    {
+        //        socket.Connect(ipPoint);
+        //        var testMess = Encoding.UTF8.GetBytes("This is test connection for server!\n");
+        //        socket.Send(testMess);
+
+        //        var data = new byte[256];
+        //        int bytes = 0;
+
+        //        var builder = new StringBuilder();
+
+        //        do
+        //        {
+        //            bytes = socket.Receive(data);
+        //            builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+
+        //        }
+        //        while (socket.Available > 0);
+
+        //        //NewMess(builder.ToString());
+
+        //        socket.Shutdown(SocketShutdown.Both);
+        //        socket.Close();
+        //        return true;
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        return false;
+        //    }
+            
+        //}
+
+        //public static bool SendAuthentification(string username, string password)
+        //{
+        //    try
+        //    {
+        //        var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //        sock.Connect(ipPoint);
+        //        var mess = Encoding.UTF8.GetBytes($@"login:{{{username}}}, password:{{{password}}}");
+
+        //        sock.Send(mess);
+
+        //        var data = new byte[256];
+        //        int bytes = 0;
+
+        //        var builder = new StringBuilder();
+
+        //        do
+        //        {
+        //            bytes = sock.Receive(data);
+        //            builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+        //        }
+        //        while (sock.Available > 0);
+
+        //        sock.Shutdown(SocketShutdown.Both);
+        //        sock.Close();
+
+        //        if (builder.ToString().Equals("login_succes"))
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        private static void Worker()
+        {
+            try
+            {
+                
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+            
+        }
+    }
+}
