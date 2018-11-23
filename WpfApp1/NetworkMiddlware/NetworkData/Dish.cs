5b08d1c82524558b2294b86197e3734702e69305
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 18:01:53 -0800

    [ADD|AdminPanel|NetworkMiddleWare] Create capsule for data transfering between server and client

---
+++ WpfApp1/NetworkMiddlware/NetworkData/Dish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.NetworkMiddlware.NetworkData
+{
+    public class Dish
+    {
+        public Guid Id { get; set; }
+        public string DishName { get; set; }
+        public string Recipe { get; set; }
+        public double OutPrice { get; set; }
+    }
+}
---
+++ WpfApp1/NetworkMiddlware/NetworkData/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.NetworkMiddlware.NetworkData
+{
+    public class Menu
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Dish> Dishes { get; set; }
+        public Cooperator Cooker { get; set; }
+    }
+}
