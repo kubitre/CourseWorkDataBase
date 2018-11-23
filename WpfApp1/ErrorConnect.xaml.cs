377381f4d9356b4de4eeea72b4616770f6eacdab
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 17:56:37 -0800

    [ADD|AdminPanel] Create window for putting errors with data aggregation from server

---
+++ WpfApp1/ErrorConnect.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace AdminPanel
+{
+    /// <summary>
+    /// Interaction logic for ErrorConnect.xaml
+    /// </summary>
+    public partial class ErrorConnect : MetroWindow
+    {
+        public ErrorConnect()
+        {
+            InitializeComponent();
+        }
+    }
+}
