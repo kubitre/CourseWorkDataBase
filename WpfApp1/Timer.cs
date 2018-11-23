d800c329095e14efdde43dc1b0d559f908dca95d
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-22 17:55:19 -0800

    [ADD|AdminPanel] Create timer for reconnection page

---
+++ WpfApp1/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel
+{
+    class Timer : INotifyPropertyChanged
+    {
+        private string timeOutP;
+
+        public string TimeOutP
+        {
+            get
+            {
+                return timeOutP;
+            }
+            set
+            {   
+
+                this.timeOutP = $" {value}" ;
+                OnProperyChanged("TimeOutP");
+            }
+        }
+
+        public void ChangeTime(int time) => this.TimeOutP = prefix + $" {time}" + postfix;
+        
+
+        public static string prefix = "Переподключение через";
+        public static string postfix = " секунд";
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnProperyChanged([CallerMemberName]string prop = "te")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
