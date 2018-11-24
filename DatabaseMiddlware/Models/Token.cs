d0a877410978818ef7f54045b8b226e115ae122c
kubitre_windows_laptop <kubitre@gmail.com>
2018-11-23 18:03:51 -0800

    [ADD|DatabaseMiddleWare] Create model of token for securied communication clients and server

---
+++ DatabaseMiddlware/Models/Token.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseMiddlware.Models
+{
+    class Token
+    {
+        public Guid Id { get; set; }
+        public User User { get; set; }
+        public Guid TokenId { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public DateTime TimeForDelete { get;set }
+    }
+}
