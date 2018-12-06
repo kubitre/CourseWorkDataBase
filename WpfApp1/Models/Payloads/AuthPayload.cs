5ea1fda019af35fdcdb26083e47f82767813d7a4
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 03:47:26 -0800

    [ADD] Create instance for payload in requesting to server

---
+++ WpfApp1/Models/Payloads/AuthPayload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Models.Payloads
+{
+    [Serializable]
+    public class AuthPayload : Payload
+    {
+        public string StatusCode { get; set; }
+        public string responseMessage { get; set; }
+    }
+}
