d0d3a9d85c4f52f5704dab5f198629c916c222ae
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 03:48:50 -0800

    [UPDATE] Create events for connections to server and update model for requseting

--- WpfApp1/NetworkMiddleware/Client.cs
+++ WpfApp1/NetworkMiddleware/Client.cs
@@ -1,19 +1,15 @@
 ﻿using AdminPanel.NetworkMiddleware.NetworkSignal;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AdminPanel.NetworkMiddleware
 {
     public static class Client
     {
         private static int _port = 10234;
-        internal static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("192.168.100.3"), _port);
+        internal static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), _port);
 
         //Signals for thread worker
         internal static ManualResetEvent connectDone =
@@ -23,13 +19,16 @@ namespace AdminPanel.NetworkMiddleware
         internal static ManualResetEvent receiveDone =
             new ManualResetEvent(false);
 
+        public delegate void AcceptException(string message);
+        public static event AcceptException GetExceptionOutput;
+
         private static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
         public delegate void Attention(string mess);
         public static event Attention NewMess;
 
 
-        public static bool RequestHandle(string nameHandler)
+        public static bool RequestHandle(string nameHandler, params object[] param)
         {
             switch (nameHandler)
             {
@@ -38,7 +37,7 @@ namespace AdminPanel.NetworkMiddleware
                         return true;
                     return false;
                 case Authentification_action.ActionType:
-                    if (new Authentification_action().Handle())
+                    if (new Authentification_action().Handle(param))
                         return true;
                     return false;
             }
@@ -52,80 +51,7 @@ namespace AdminPanel.NetworkMiddleware
             worker.Start();
         }
 
-        //public static bool TestConnection()
-        //{
-        //    try
-
-        //    {
-        //        socket.Connect(ipPoint);
-        //        var testMess = Encoding.UTF8.GetBytes("This is test connection for server!\n");
-        //        socket.Send(testMess);
-
-        //        var data = new byte[256];
-        //        int bytes = 0;
-
-        //        var builder = new StringBuilder();
-
-        //        do
-        //        {
-        //            bytes = socket.Receive(data);
-        //            builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
-
-        //        }
-        //        while (socket.Available > 0);
-
-        //        //NewMess(builder.ToString());
-
-        //        socket.Shutdown(SocketShutdown.Both);
-        //        socket.Close();
-        //        return true;
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        return false;
-        //    }
-            
-        //}
-
-        //public static bool SendAuthentification(string username, string password)
-        //{
-        //    try
-        //    {
-        //        var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        //        sock.Connect(ipPoint);
-        //        var mess = Encoding.UTF8.GetBytes($@"login:{{{username}}}, password:{{{password}}}");
-
-        //        sock.Send(mess);
-
-        //        var data = new byte[256];
-        //        int bytes = 0;
-
-        //        var builder = new StringBuilder();
-
-        //        do
-        //        {
-        //            bytes = sock.Receive(data);
-        //            builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
-        //        }
-        //        while (sock.Available > 0);
-
-        //        sock.Shutdown(SocketShutdown.Both);
-        //        sock.Close();
-
-        //        if (builder.ToString().Equals("login_succes"))
-        //        {
-        //            return true;
-        //        }
-        //        else
-        //        {
-        //            return false;
-        //        }
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        return false;
-        //    }
-        //}
+        public static void GetExceptionHandle(string message) => GetExceptionOutput(message);
 
         private static void Worker()
         {
