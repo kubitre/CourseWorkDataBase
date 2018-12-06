f5193e4f072cd7a2c1979d03c29f2a39b645be65
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 03:41:28 -0800

    [UPDATE|REFACTOR]View for this tasks

--- WpfApp1/Dish.xaml.cs
+++ WpfApp1/Dish.xaml.cs
@@ -1,27 +1,48 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
-namespace WpfApp1
+namespace AdminPanel
 {
     /// <summary>
     /// Interaction logic for Dish.xaml
     /// </summary>
-    public partial class Dish : Window
+    public partial class Dish : MetroWindow
     {
         public Dish()
         {
             InitializeComponent();
+            DataContext = new ViewModel.DishViewModel();
+        }
+
+        private void DishData_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            (DataContext as ViewModel.DishViewModel).Dishes.Add(new Models.Dish { Name = "Борщ", Recipe = "Тут какой-то рецепт", OutPrice = "123", Id = Guid.NewGuid() });       
+        }
+
+        private void ChangeElement_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveElement_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNewElement_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var windowAddingNewElement = new DishAdd();
+            windowAddingNewElement.ShowDialog();
+            
+        }
+
+        private void TraceRoute_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var windowMain = new MainWindow();
+            windowMain.Show();
+            this.Close();
         }
     }
 }
---
+++ WpfApp1/DishAdd.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace AdminPanel
+{
+    /// <summary>
+    /// Interaction logic for DishAdd.xaml
+    /// </summary>
+    public partial class DishAdd : MetroWindow
+    {
+        public DishAdd()
+        {
+            InitializeComponent();
+        }
+    }
+}
