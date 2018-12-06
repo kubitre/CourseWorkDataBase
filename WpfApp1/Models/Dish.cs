adc9d09848d5dcb49d93208fe3b52beb925c1017
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 03:46:43 -0800

    [ADD] Create dish model for output on view

---
+++ WpfApp1/Models/Dish.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Models
+{
+    public class Dish : INotifyPropertyChanged
+    {
+        private Guid id;
+        private string name;
+        private string recipe;
+        private string outPrice;
+
+        public Guid Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Recipe
+        {
+            get
+            {
+                return recipe;
+            }
+            set
+            {
+                recipe = value;
+                OnPropertyChanged("Recipe");
+            }
+        }
+
+        public string OutPrice
+        {
+            get
+            {
+                return outPrice;
+            }
+            set
+            {
+                outPrice = value;
+                OnPropertyChanged("OutPrice");
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
