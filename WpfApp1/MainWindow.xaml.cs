a995c4f977935580df0396df4fd70ea57068433f
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 03:54:53 -0800

    [ADD|UPDATE] Create handlers for view events and create binding model for data context

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -8,5 +8,49 @@ namespace AdminPanel
         {
             InitializeComponent();
         }
+
+        private void CooperatorsWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.MainWindowSSSS.Children.Clear();
+        }
+
+        private void DishesWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var windowDishes = new Dish();
+            windowDishes.Show();
+            this.Close();
+        }
+
+        private void MenusWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var windowMenu = new Menu();
+            windowMenu.Show();
+            this.Hide();
+        }
+
+        private void UsersWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void SettingButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void exitFromSession_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void MySettingsItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
     }
 }
