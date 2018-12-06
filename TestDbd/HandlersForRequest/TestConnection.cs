90ce6c1c8a30d96c543a516352d3d930f2f3354f
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 04:08:15 -0800

    [ADD] Create testconnection handler

--- TestDbd/HandlersForRequest/TestConnection.cs
+++ TestDbd/HandlersForRequest/TestConnection.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ServerDB.ServerData;
+using System;
 using System.Text;
 
 namespace ServerDB.HandlersForRequest
 {
     class TestConnection : IHandler
     {
-        public void hand()
+        public void hand(StateObject state)
         {
-            throw new NotImplementedException();
+            var bytes = Encoding.UTF8.GetBytes("connected succes!");
+            try
+            {
+                state.WorkSocket.BeginSend(bytes, 0, bytes.Length, 0, new AsyncCallback(ServerHandlers.SendCallBack.SendCallback), state.WorkSocket);
+
+                ServerDb.Server.allDone.Set();
+            }
+            catch(Exception ex)
+            {
+                ServerDb.Server.NewExceptionRequest(ex.Message);
+            }
+            
+
         }
     }
 }
