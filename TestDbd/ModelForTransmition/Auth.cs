7b832234e525d356e8e31ea033327af4f9f5b7ef
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 04:09:44 -0800

    [ADD] Create model for auth handle

---
+++ TestDbd/ModelForTransmition/Auth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerDB.ModelForTransmition
+{
+    [Serializable]
+    public class Auth
+    {
+        public string Username;
+        public string Password;
+    }
+}
