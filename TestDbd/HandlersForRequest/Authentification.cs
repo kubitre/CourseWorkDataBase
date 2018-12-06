16e1ccc410e754f945fd4f87bb594c7b37ac144a
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 04:09:23 -0800

    [ADD] Create model for authentification response, add ssl middleware for request handle

--- TestDbd/HandlersForRequest/Authentification.cs
+++ TestDbd/HandlersForRequest/Authentification.cs
@@ -1,17 +1,45 @@
-﻿namespace ServerDB.HandlersForRequest
+﻿using ServerDB.ServerData;
+using DatabaseMiddlware.Workers.Users;
+using ServerDB.ModelForTransmition;
+using System;
+
+namespace ServerDB.HandlersForRequest
 {
     class Authentification : IHandler
     {
-        private string payload = "";
+        public static string ActionType = "authentification";
+        public string Action = ActionType;
+        public string Payload = "";
+
+        private static string PatternForCondition = "";
+
+        public Authentification() { }
 
         public Authentification(string payload)
         {
-            this.payload = payload;
+            this.Payload = payload;
         }
 
-        public void hand()
+        public void hand(StateObject state)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var authTok = (Auth)Json.JsonParser.Deserialize(state.MessageForReciveData.ToString());
+
+                if (UserHandler.Authentificate(authTok.Username, authTok.Password))
+                {
+                    ServerHandlers.SendHandler.Send(state.WorkSocket, $"authentificate succes! token : {Guid.NewGuid()}");
+                }
+                else
+                {
+                    ServerHandlers.SendHandler.Send(state.WorkSocket, "authentificate error! error code: User does not exist in database!");
+                }
+            }
+            catch(Exception ex)
+            {
+                ServerDb.Server.NewExceptionRequest(ex.Message);
+                ServerHandlers.SendHandler.Send(state.WorkSocket, "authentificate error! Does not prepare recieved data !");
+            }
         }
     }
 }
