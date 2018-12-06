4bef41595e8fa2c69155f7be9fcc25cf0faa2615
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-06 03:43:00 -0800

    [ADD] Create object for inline working

---
+++ WpfApp1/StateObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace AdminPanel.NetworkMiddlware
+{
+    class StateObject
+    {
+        public Socket WorkSocket;
+        public const int BufferSize = 256;
+        public byte[] buffer = new byte[BufferSize];
+        public StringBuilder MessageForRecieving = new StringBuilder();
+    }
+}
