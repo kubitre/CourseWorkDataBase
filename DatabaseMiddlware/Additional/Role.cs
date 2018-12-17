824ae7603f10b22d731238faaa2db2c2de3d1f61
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 17:06:01 -0800

    [ADD] Create role model for this app

--- DatabaseMiddlware/Additional/Role.cs
+++ DatabaseMiddlware/Additional/Role.cs
@@ -7,5 +7,20 @@
             Programmer = 1,
             Calculator = 2
         }
+
+        public static string GetRole(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return Type.Administrator.ToString();
+                case 1:
+                    return Type.Programmer.ToString();
+                case 2:
+                    return Type.Calculator.ToString();
+                default:
+                    return "";
+            }
+        }
     }
 }
