3dec1d2fe87ec559833a253fdc142b150dee078d
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-17 10:13:17 -0800

    [ADD] Create viewmodels for view and model for category and position

---
+++ WpfApp1/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.ViewModel
+{
+    public class CategoryViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Models.Category> Categories { get; set; }
+        private Models.Category selectedCategory;
+
+        public Models.Category SelectedCategory
+        {
+            get
+            {
+                return selectedCategory;
+            }
+            set
+            {
+                selectedCategory = value;
+                OnPropertyChanged("SelectedCategory");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CategoryViewModel()
+        {
+            this.Categories = new ObservableCollection<Models.Category>();
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/PositionViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AdminPanel.ViewModel
+{
+    public class PositionViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Models.Position> Positions { get; set; }
+        private Models.Position selectedPosition;
+        
+        public Models.Position SelectedPosition
+        {
+            get
+            {
+                return selectedPosition;
+            }
+            set
+            {
+                selectedPosition = value;
+                OnPropertyChanged("SelectedPosition");
+            }
+        }
+
+        public PositionViewModel()
+        {
+            this.Positions = new ObservableCollection<Models.Position>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
