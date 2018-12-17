990d8cec83185248d539a056cb0f82b5d5b78897
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:44:50 -0800

    [UPDATE] Network sending and recieving

--- WpfApp1/ErrorConnect.xaml.cs
+++ WpfApp1/ErrorConnect.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MahApps.Metro.Controls;
+﻿using MahApps.Metro;
+using MahApps.Metro.Controls;
 using System.Windows;
 
 namespace AdminPanel
@@ -8,9 +9,25 @@ namespace AdminPanel
     /// </summary>
     public partial class ErrorConnect : MetroWindow
     {
+        private ApplicationMemory.MemoryBuild _memory;
+
         public ErrorConnect()
         {
             InitializeComponent();
+        }
+
+        public void SetMemoryDump(ApplicationMemory.MemoryBuild memory)
+        {
+            this._memory = memory;
+            ThemeManager.ChangeAppStyle(this,
+                                        ThemeManager.GetAccent(this._memory.GetAppAccentTheme()),
+                                        ThemeManager.GetAppTheme(this._memory.GetAppTheme()));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this._memory.AddToHistory("LoginPage");
+            this.DialogResult = true;
         }
     }
 }
