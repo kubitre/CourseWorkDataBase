1757a11f4b3b93bc80d9f19e55f6f2b319055d11
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:59:24 -0800

    [ADD] Create needs entities for server

---
+++ TestDbd/InternalNeeds/Dishes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerDb.InternalNeeds
+{
+    public class Dishes
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        List<DishBlocks> dishBlokcs { get; set; }
+    }
+}
