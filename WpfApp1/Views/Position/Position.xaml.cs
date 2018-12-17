7addcf7a1d5e440ca854e8f9a939272c12dc670b
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:39:06 -0800

    [ADD] Create position view for position model

---
+++ WpfApp1/Views/Position/Position.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace AdminPanel.Views.Position
+{
+    /// <summary>
+    /// Interaction logic for Position.xaml
+    /// </summary>
+    public partial class Position : MetroWindow
+    {
+        public Position()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp1/Views/Position/PositionAdd.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace AdminPanel.Views.Position
+{
+    /// <summary>
+    /// Interaction logic for PositionAdd.xaml
+    /// </summary>
+    public partial class PositionAdd : MetroWindow
+    {
+        public PositionAdd()
+        {
+            InitializeComponent();
+        }
+    }
+}
