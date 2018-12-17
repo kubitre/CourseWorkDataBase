8e5b6013da5449660f3c869c63f412d4a53587c4
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:38:13 -0800

    [ADD] Create cooperator view for model cooperator

---
+++ WpfApp1/Views/Cooperator/Cooperator.xaml.cs
@@ -0,0 +1,115 @@
+﻿using AdminPanel.ViewModel;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdminPanel.Views.Cooperator
+{
+    
+    /// <summary>
+    /// Interaction logic for Cooperator.xaml
+    /// </summary>
+    public partial class Cooperator : MetroWindow
+    {
+        private ApplicationMemory.MemoryBuild _memory;
+        private NetworkMiddleware.Client _clientNetwork;
+        public Cooperator()
+        {
+            InitializeComponent();
+            this.DataContext = new CooperatorViewModel();
+            this._clientNetwork = new NetworkMiddleware.Client();
+        }
+
+        public void SetMemoryDump(ApplicationMemory.MemoryBuild memory)
+        {
+            this._memory = memory;
+            ThemeManager.ChangeAppStyle(this,
+                                        ThemeManager.GetAccent(this._memory.GetAppAccentTheme()),
+                                        ThemeManager.GetAppTheme(this._memory.GetAppTheme()));
+        }
+
+        private void ChangeElement_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveElement_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TraceRoute_Click(object sender, RoutedEventArgs e)
+        {
+            var windowMain = new MainWindow();
+            this._memory.AddToHistory("MainWindow");
+            windowMain.SetMemoryDump(this._memory);
+            windowMain.Show();
+            this.Close();
+        }
+
+        private void AddNewElement_Click(object sender, RoutedEventArgs e)
+        {
+            var windowForAdding = new CooperatorAdd();
+            this._memory.AddToHistory("CooperatorAdd");
+            windowForAdding.SetMemoryDump(this._memory);
+            windowForAdding.ShowDialog();
+        }
+
+        private void CooperatorData_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            this._clientNetwork.RequestHandle(NetworkMiddleware.NetworkResponseCodes.CooperatorCodes.COOPERATOR_GET_CODE, 100);
+            if(this._clientNetwork.Response != null)
+            {
+                var response = Newtonsoft.Json.JsonConvert.DeserializeObject<NetworkMiddleware.NetworkData.ReponseAllRequests>(this._clientNetwork.Response);
+                foreach(var element in Newtonsoft.Json.JsonConvert.DeserializeObject<List<NetworkMiddleware.NetworkData.Cooperator>>(response.Reponse))
+                {
+                    (this.DataContext as ViewModel.CooperatorViewModel).Cooperators.Add(new AdminPanel.Models.Cooperator
+                    {
+                        Id = element.Id,
+                        Address = $"{element.Street} {element.Building}, кв. {element.Flat}",
+                        Position = $"{element.Position} {element.Category}",
+                        Price = element.Salary,
+                        FMEName = $"{element.FirstName} {element.MiddleName[0]}. {element.LastName[0]}."
+                    });
+                }
+            }
+        }
+
+        private void SettingsPanels_Click(object sender, RoutedEventArgs e)
+        {
+            this.SettingsPanel.IsOpen = true;
+        }
+
+        private void AboutApplication_Click(object sender, RoutedEventArgs e)
+        {
+            this.AboutApp.IsOpen = true;
+        }
+
+        private void AboutMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.AboutMe.IsOpen = true;
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/Views/Cooperator/CooperatorAdd.xaml.cs
@@ -0,0 +1,121 @@
+﻿using MahApps.Metro;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Collections.Generic;
+
+namespace AdminPanel.Views.Cooperator
+{
+    /// <summary>
+    /// Interaction logic for CooperatorAdd.xaml
+    /// </summary>
+    public partial class CooperatorAdd : MetroWindow
+    {
+        private ApplicationMemory.MemoryBuild _memory;
+
+        public CooperatorAdd()
+        {
+            InitializeComponent();
+
+        }
+        public void SetMemoryDump(ApplicationMemory.MemoryBuild memory)
+        {
+            this._memory = memory;
+            ThemeManager.ChangeAppStyle(this,
+                                        ThemeManager.GetAccent(this._memory.GetAppAccentTheme()),
+                                        ThemeManager.GetAppTheme(this._memory.GetAppTheme()));
+
+            var clientNetwork = new NetworkMiddleware.Client();
+            if(clientNetwork.RequestHandle(NetworkMiddleware.NetworkResponseCodes.StreetCodes.STREET_GET_CODE, 100))
+            {
+                var response = Newtonsoft.Json.JsonConvert.DeserializeObject<NetworkMiddleware.NetworkData.ReponseAllRequests>(clientNetwork.Response);
+                foreach(var element in Newtonsoft.Json.JsonConvert.DeserializeObject<List<NetworkMiddleware.NetworkData.Street>>(response.Reponse))
+                {
+                    this.Addresses.Items.Add(element.Name);
+                }
+            }
+
+            var clientNetworkPosition = new NetworkMiddleware.Client();
+            if(clientNetworkPosition.RequestHandle(NetworkMiddleware.NetworkResponseCodes.PositionCodes.POSITION_GET_CODE, 100))
+            {
+                var response = Newtonsoft.Json.JsonConvert.DeserializeObject<NetworkMiddleware.NetworkData.ReponseAllRequests>(clientNetworkPosition.Response);
+                foreach(var element in Newtonsoft.Json.JsonConvert.DeserializeObject<List<NetworkMiddleware.NetworkData.Position>>(response.Reponse))
+                {
+                    this.PositonCombo.Items.Add(element.Name);
+                }
+            }
+
+            var clientNetworkCategory = new NetworkMiddleware.Client();
+            if (clientNetworkCategory.RequestHandle(NetworkMiddleware.NetworkResponseCodes.CategoryCodes.CATEGORY_GET_CODE, 100))
+            {
+                var response = Newtonsoft.Json.JsonConvert.DeserializeObject<NetworkMiddleware.NetworkData.ReponseAllRequests>(clientNetworkCategory.Response);
+                foreach (var element in Newtonsoft.Json.JsonConvert.DeserializeObject<List<NetworkMiddleware.NetworkData.Category>>(response.Reponse))
+                {
+                    this.CategoryCombo.Items.Add(element.Name);
+                }
+            }
+        }
+
+        private void CreateNewCooperator_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (
+                !ChekcInput(this.FirstName_Input.Text, this.MiddleName_Input.Text, this.LastName_Input.Text, this.Building_Input.Text, this.Flat_Input.Text, this.salary_input.Text)
+                )
+            {
+                this.ShowMessageAsync("Ошибка!", "Вы не ввели обязательные поля для ввода!");
+            }
+            if(this.Addresses.SelectedItem == null)
+                this.ShowMessageAsync("Ошибка!", "Вы не выбрали улицу!");
+
+            if(this.PositonCombo.SelectedItem == null)
+                this.ShowMessageAsync("Ошибка!", "Вы не выбрали должность!");
+
+
+            var cooperator = new NetworkMiddleware.NetworkData.Cooperator
+            {
+                FirstName = this.FirstName_Input.Text,
+                MiddleName = this.MiddleName_Input.Text,
+                LastName = this.LastName_Input.Text,
+                Street = this.Addresses.SelectedItem.ToString(),
+                Salary = double.Parse(this.salary_input.Text),
+                BirthDay = this.BirthDay.DisplayDate,
+                Building = int.Parse(this.Building_Input.Text),
+                Category = this.CategoryCombo.SelectedItem.ToString(),
+                Flat = int.Parse(this.Flat_Input.Text),
+                Position = this.PositonCombo.SelectedItem.ToString()
+            };
+            
+        }
+
+        private bool ChekcInput(params string[] param)
+        {
+            foreach(var element in param)
+            {
+                if (string.IsNullOrEmpty(element) | string.IsNullOrWhiteSpace(element))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ListWithAdress_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void Addresses_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void PositonCombo_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CategoryCombo_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
