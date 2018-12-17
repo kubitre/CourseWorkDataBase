e910726a48d24d6e475fa9d04ebec0220c01df0e
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:38:32 -0800

    [ADD] Create dish view dor model dish

---
+++ WpfApp1/Views/Dish/Dish.xaml.cs
@@ -0,0 +1,100 @@
+﻿using AdminPanel.NetworkMiddleware;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace AdminPanel.Views.Dish
+{
+    public partial class Dish : MetroWindow
+    {
+        private ApplicationMemory.MemoryBuild _memory;
+        private Client _networkClient;
+
+        public static event Events.EventForDataForm.AddNewData<NetworkMiddleware.NetworkData.Dish> Dishes;
+        public Dish()
+        {
+            InitializeComponent();
+            DataContext = new ViewModel.DishViewModel();
+            this._networkClient = new Client();
+            Dishes += UpdateDataOnForm;
+            (this.DishData as DataGrid).RowHeight = 100;
+        }
+
+        public void SetMemoryDump(ApplicationMemory.MemoryBuild memory)
+        {
+            this._memory = memory;
+            ThemeManager.ChangeAppStyle(this,
+                                        ThemeManager.GetAccent(this._memory.GetAppAccentTheme()),
+                                        ThemeManager.GetAppTheme(this._memory.GetAppTheme()));
+        }
+
+        private void DishData_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this._networkClient.RequestHandle(NetworkMiddleware.NetworkResponseCodes.DishCodes.DISH_GET_CODE, 100);
+            if(this._networkClient.State.LastResponse != null)
+                foreach (var elementRecieved in (Newtonsoft.Json.JsonConvert.DeserializeObject<List<NetworkMiddleware.NetworkData.Dish>>(this._networkClient.State.LastResponse)))
+                    (this.DataContext as ViewModel.DishViewModel).Dishes.Add(new Models.Dish() { Id = elementRecieved.Id, Name= elementRecieved.Name, OutPrice = elementRecieved.Outer.ToString(), Recipe = string.Join("\n", elementRecieved.Products.ToArray())});
+        }
+
+        private void ChangeElement_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.ShowMessageAsync("Изменение элемента", $"ID: {(this.DataContext as ViewModel.DishViewModel).SelectedDish.Id}; Name: {(this.DataContext as ViewModel.DishViewModel).SelectedDish.Name}");
+        }
+
+        private void RemoveElement_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNewElement_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var windowAddingNewElement = new DishAdd();
+            this._memory.AddToHistory("DishAdd");
+            windowAddingNewElement.SetMemoryDump(this._memory);
+            windowAddingNewElement.ShowDialog();
+            
+        }
+
+        private void TraceRoute_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var windowMain = new MainWindow();
+            this._memory.AddToHistory("MainWindow");
+            windowMain.SetMemoryDump(this._memory);
+            windowMain.Show();
+            this.Close();
+        }
+
+        public static void UpdateDataOnForm(List<NetworkMiddleware.NetworkData.Dish> dishes)
+        {
+
+        }
+
+        private void SettingsPanels_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.SettingsPanel.IsOpen = true;
+        }
+
+        private void AboutApplication_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.AboutApp.IsOpen = true;
+        }
+
+        private void AboutMeButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.AboutMe.IsOpen = true;
+        }
+
+        private void Refresh_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/Views/Dish/DishAdd.xaml.cs
@@ -0,0 +1,103 @@
+﻿using MahApps.Metro;
+using MahApps.Metro.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace AdminPanel.Views.Dish
+{
+    public partial class DishAdd : MetroWindow
+    {
+        private ApplicationMemory.MemoryBuild _memory;
+        private List<NetworkMiddleware.NetworkData.Product> _selectedProducts;
+
+        public DishAdd()
+        {
+            InitializeComponent();
+
+            this.DataContext = new AdminPanel.ViewModel.ProductViewModel();
+
+            this._selectedProducts = new List<NetworkMiddleware.NetworkData.Product>();
+
+            var clientNetwork = new NetworkMiddleware.Client();
+            if(clientNetwork.RequestHandle(NetworkMiddleware.NetworkResponseCodes.ProductCodes.PRODUCT_GET_CODE, 100))
+            {
+                var response = Newtonsoft.Json.JsonConvert.DeserializeObject<NetworkMiddleware.NetworkData.ReponseAllRequests>(clientNetwork.Response);
+                foreach (var element in Newtonsoft.Json.JsonConvert.DeserializeObject<List<NetworkMiddleware.NetworkData.Product>>(response.Reponse))
+                {
+                    this.ChooseProducts.Items.Add(element.Name);
+                    this._selectedProducts.Add(element);
+                }
+                
+            }
+
+            var clientNetwork2 = new NetworkMiddleware.Client();
+            if(clientNetwork2.RequestHandle(NetworkMiddleware.NetworkResponseCodes.CooperatorCodes.COOPERATOR_GET_CODE, 100))
+            {
+                var response = Newtonsoft.Json.JsonConvert.DeserializeObject<NetworkMiddleware.NetworkData.ReponseAllRequests>(clientNetwork2.Response);
+                foreach(var element in Newtonsoft.Json.JsonConvert.DeserializeObject<List<NetworkMiddleware.NetworkData.Cooperator>>(response.Reponse))
+                    this.CookcerList.Items.Add(element.FirstName);
+                
+            }
+        }
+
+        public void SetMemoryDump(ApplicationMemory.MemoryBuild memory)
+        {
+            this._memory = memory;
+            ThemeManager.ChangeAppStyle(this,
+                                        ThemeManager.GetAccent(this._memory.GetAppAccentTheme()),
+                                        ThemeManager.GetAppTheme(this._memory.GetAppTheme()));
+        }
+
+
+        private void RemoveElement_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var element = (this.DataContext as ViewModel.ProductViewModel).SelectedProduct;
+            (this.DataContext as ViewModel.ProductViewModel).Products.Remove((this.DataContext as ViewModel.ProductViewModel)
+                .Products.FirstOrDefault(x => x.Name.Equals(element.Name)
+                ));
+        }
+
+        private bool ChekcInput(params string[] param)
+        {
+            foreach (var element in param)
+            {
+                if (string.IsNullOrEmpty(element) | string.IsNullOrWhiteSpace(element))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ProductList_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void AddNewDish_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void CookcerList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ChooseProducts_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            foreach(var product in this._selectedProducts)
+                if(product.Name.Equals((sender as ComboBox).SelectedValue))
+                    (this.DataContext as ViewModel.ProductViewModel).Products.Add(new Models.Product
+                    {
+                        Id = product.Id,
+                        Name = product.Name,
+                        Price = product.Price
+                    });
+
+            (sender as ComboBox).SelectedItem = null;
+        }
+    }
+}
