f41e2e61142c4c815bbdfe6b9202802b5c27bbc0
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:43:21 -0800

    [UPDATE] Change previous settings of our application

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,14 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Tuple<AppTheme, Accent> appStyles = ThemeManager.DetectAppStyle(Application.Current);
+
+            ThemeManager.ChangeAppStyle(Application.Current,
+                                        ThemeManager.GetAccent("Cobalt"),
+                                        ThemeManager.GetAppTheme("BaseDark"));
+            base.OnStartup(e);
+        }
     }
 }
