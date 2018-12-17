05b0a351dca70dab08fd80b46d2dbdf34768743f
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:42:48 -0800

    [ADD] Create viewmodel for view and model of user

---
+++ WpfApp1/ViewModel/UserViewModel.cs
@@ -0,0 +1,38 @@
+﻿using AdminPanel.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AdminPanel.ViewModel
+{
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        private User selectedUser;
+        public ObservableCollection<User> Users { get; set; }
+
+        public User SelectedUser
+        {
+            get
+            {
+                return selectedUser;
+            }
+            set
+            {
+                selectedUser = value;
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+
+        public UserViewModel()
+        {
+            this.Users = new ObservableCollection<User>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
