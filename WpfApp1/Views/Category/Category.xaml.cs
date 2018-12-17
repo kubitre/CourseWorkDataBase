d8f0a215851aaf2555097521029c29541e9a5682
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:37:36 -0800

    [ADD] Create category view for model category

---
+++ WpfApp1/Views/Category/Category.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace AdminPanel.Views.Category
+{
+    /// <summary>
+    /// Interaction logic for Category.xaml
+    /// </summary>
+    public partial class Category : MetroWindow
+    {
+        public Category()
+        {
+            InitializeComponent();
+        }
+    }
+}
