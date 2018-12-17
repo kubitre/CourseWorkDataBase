b23274c78aeaafc8c370dacb39bee66092d3245c
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:42:08 -0800

    [ADD] Create viewmodel for model and view of product

---
+++ WpfApp1/ViewModel/ProductViewModel.cs
@@ -0,0 +1,38 @@
+﻿using AdminPanel.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AdminPanel.ViewModel
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        private Product selectedProduct;
+        public ObservableCollection<Product> Products { get; set; }
+
+        public Product SelectedProduct
+        {
+            get
+            {
+                return selectedProduct;
+            }
+            set
+            {
+                selectedProduct = value;
+                OnPropertyChanged("SelectedProduct");
+            }
+        }
+
+        public ProductViewModel()
+        {
+            this.Products = new ObservableCollection<Product>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
