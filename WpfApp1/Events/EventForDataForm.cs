6b14ba30d2d14aecab4859449856fd578d56ef68
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:47:09 -0800

    [ADD] Create events for this application

---
+++ WpfApp1/Events/EventForDataForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Events
+{
+    public static class EventForDataForm
+    {
+
+        public delegate void AddNewData<T>(List<T> data);
+    }
+}
