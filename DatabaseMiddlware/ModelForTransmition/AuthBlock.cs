7abd90994f154ee9615dd317a75967d0729a491c
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 17:07:52 -0800

    [ADD|UPDATE] Model for transmition

---
+++ DatabaseMiddlware/ModelForTransmition/AuthBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseMiddlware.ModelForTransmition
+{
+    [Serializable]
+    public class AuthBLock
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime Enter { get; set; }
+    }
+}
---
+++ DatabaseMiddlware/ModelForTransmition/DishBlock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseMiddlware.ModelForTransmition
+{
+    [Serializable]
+    public class DishBlock
+    {
+        public int countDishes { get; set; }
+        public int offsetAboutFirst { get; set; }
+    }
+}
