024452124d12e54f90f12d3c626603daa14c347b
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:41:52 -0800

    [ADD] Create viewmodel for view and model of menu

---
+++ WpfApp1/ViewModel/MenuViewModel.cs
@@ -0,0 +1,43 @@
+﻿using AdminPanel.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.ViewModel
+{
+    public class MenuViewModel : INotifyPropertyChanged
+    {
+        private Menu selectedMenu;
+        public ObservableCollection<Menu> Menus { get; set; }
+
+        public Menu SelectedMenu
+        {
+            get
+            {
+                return selectedMenu;
+            }
+            set
+            {
+                selectedMenu = value;
+                OnPropertyChanged("SelectedMenu");
+            }
+        }
+
+        public MenuViewModel()
+        {
+            this.Menus = new ObservableCollection<Menu>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
