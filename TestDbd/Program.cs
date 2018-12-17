2fc5e6a859a65e186f3577fc3e03385199bf7500
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 17:02:12 -0800

    [ADD] Create runnig test data filling

--- TestDbd/Program.cs
+++ TestDbd/Program.cs
@@ -7,7 +7,12 @@ namespace ServerDb
         static void Main(string[] args)
         {
 
-            DatabaseMiddlware.Class1.CreateTestUser();
+            //DatabaseMiddlware.Class1.CreateTestUser();
+            //new DatabaseMiddlware.Class1().RunFil();
+            //new DatabaseMiddlware.Class1().FillProductAndDish();
+            //new DatabaseMiddlware.Class1().FillTestDishes();
+
+            //new DatabaseMiddlware.Class1().FillMenu();
             var server = new Server();
             server.Run();
         }
