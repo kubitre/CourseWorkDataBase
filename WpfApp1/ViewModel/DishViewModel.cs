ac1fe49a694804073e088125628f82e29891a1d8
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:41:30 -0800

    [ADD] Create viewmodel for view and model of dish

---
+++ WpfApp1/ViewModel/DishViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.ViewModel
+{
+    public class DishViewModel: INotifyPropertyChanged
+    {
+        private Models.Dish selectedDish;
+        public ObservableCollection<Models.Dish> Dishes { get; set; }
+
+        public Models.Dish SelectedDish
+        {
+            get
+            {
+                return selectedDish;
+            }
+            set
+            {
+                selectedDish = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public DishViewModel()
+        {
+            Dishes = new ObservableCollection<Models.Dish>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
