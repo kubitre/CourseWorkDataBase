b73c52ec5e955b9a82e9c258dfd87d18794ddfe1
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 17:02:50 -0800

    [UPDATE]

--- TestDbd/Server.cs
+++ TestDbd/Server.cs
@@ -48,7 +48,7 @@ namespace ServerDb
 
                     Console.WriteLine($"[LOG {DateTime.Now}]: Waiting connection for this server");
 
-                    this.TcpChannel.BeginAccept(new AsyncCallback(ServerDB.ServerHandlers.AcceptHandler.AcceptCallback), this.TcpChannel);
+                    this.TcpChannel.BeginAccept(new AsyncCallback(ServerDb.ServerHandlers.AcceptHandler.AcceptCallback), this.TcpChannel);
 
                     allDone.WaitOne();
                 }
@@ -69,70 +69,70 @@ namespace ServerDb
             Console.WriteLine($"[LOG {DateTime.Now}]: New Exception handle! exception code: {code}");
         }
 
-        //private void PrepareRequest(Socket socket, string message)
-        //{
-        //    var regLogin = new Regex("(login:{[a-zA-Z]{4,}[0-9]{2,}})");
-        //    var regPassword = new Regex("(password:{[a-zA-Z0-9]{6,30}})");
+        private void PrepareRequest(Socket socket, string message)
+        {
+            var regLogin = new Regex("(login:{[a-zA-Z]{4,}[0-9]{2,}})");
+            var regPassword = new Regex("(password:{[a-zA-Z0-9]{6,30}})");
 
-        //    Console.WriteLine("Check Login and Password");
+            Console.WriteLine("Check Login and Password");
 
-        //    if (message.Contains("login") & message.Contains("password"))
-        //    {
-        //        Console.WriteLine("message contain login and password");
-        //        var mathcesLogin = regLogin.Matches(message);
-        //        var mathcersPassword = regPassword.Matches(message);
+            if (message.Contains("login") & message.Contains("password"))
+            {
+                Console.WriteLine("message contain login and password");
+                var mathcesLogin = regLogin.Matches(message);
+                var mathcersPassword = regPassword.Matches(message);
 
-        //        if(mathcesLogin.Count == 1 & mathcersPassword.Count == 1)
-        //        {
+                if(mathcesLogin.Count == 1 & mathcersPassword.Count == 1)
+                {
 
-        //            Console.WriteLine("Data is available!");
-        //            var regLoginValue = new Regex("[a-zA-Z]{4,}[0-9]{2,}}");
-        //            var regPasswordValue = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+                    Console.WriteLine("Data is available!");
+                    var regLoginValue = new Regex("[a-zA-Z]{4,}[0-9]{2,}}");
+                    var regPasswordValue = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
 
-        //            if(regLoginValue.Matches(mathcesLogin[0].Value).Count == 1 & regPasswordValue.Matches(mathcersPassword[0].Value).Count == 1)
-        //            {
-        //                Console.WriteLine("Work with db!");
-        //                WorkWithAuthentificate(socket, regLoginValue.Matches(mathcesLogin[0].Value)[0].Value, regPasswordValue.Matches(mathcersPassword[0].Value)[0].Value);
-        //            }
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("Data is not available!");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("message not contain login and password!");
-        //        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    if(regLoginValue.Matches(mathcesLogin[0].Value).Count == 1 & regPasswordValue.Matches(mathcersPassword[0].Value).Count == 1)
+                    {
+                        Console.WriteLine("Work with db!");
+                        WorkWithAuthentificate(socket, regLoginValue.Matches(mathcesLogin[0].Value)[0].Value, regPasswordValue.Matches(mathcersPassword[0].Value)[0].Value);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Data is not available!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("message not contain login and password!");
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 
-        //    }
-        //}
+            }
+        }
 
-        //private void WorkWithAuthentificate(Socket socket, string username, string password)
-        //{
-        //    Console.WriteLine("Authentificate processing");
+        private void WorkWithAuthentificate(Socket socket, string username, string password)
+        {
+            Console.WriteLine("Authentificate processing");
 
-        //    //var sendMessage = new AuthentificateSignal();
+            //var sendMessage = new AuthentificateSignal();
 
-        //    //if (DatabaseMiddlware.Workers.Users.UserHandler.Authentificate(username, password))
-        //    //    sendMessage.Setting("login_succes");
-        //    //else
-        //    //    sendMessage.Setting("login_error");
-
-
-
-        //    //var messgeByte = Encoding.UTF8.GetBytes(Json.JsonParser.Serialize(sendMessage));
-
-        //    //socket.Send(messgeByte);
-        //    //ShutDown(socket);
-        //}
+            //if (DatabaseMiddlware.Workers.Users.UserHandler.Authentificate(username, password))
+            //    sendMessage.Setting("login_succes");
+            //else
+            //    sendMessage.Setting("login_error");
 
 
-        //private void ShutDownSocket(Socket socket)
-        //{
-        //    socket.Shutdown(SocketShutdown.Both);
-        //    socket.Close();
-        //}
+
+            //var messgeByte = Encoding.UTF8.GetBytes(Json.JsonParser.Serialize(sendMessage));
+
+            //socket.Send(messgeByte);
+            //ShutDown(socket);
+        }
+
+
+        private void ShutDownSocket(Socket socket)
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
 
     }
 }
