40cc6b13d89ce6546ddb77c5db34e74a05fcfc66
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 17:06:46 -0800

    [ADD] Create and update for internal needs

---
+++ DatabaseMiddlware/InternalNeeds/DishBlocks.cs
@@ -0,0 +1,13 @@
+﻿using DatabaseMiddlware.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseMiddlware.InternalNeeds
+{
+    public class DishBlocks
+    {
+        public Product product;
+        public double AmountInDish;
+    }
+}
