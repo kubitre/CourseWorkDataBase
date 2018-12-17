85bbf054668af089b8dc72da114804058f57478e
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 16:42:26 -0800

    [ADD] Create viewmodel for view and model of street

---
+++ WpfApp1/ViewModel/StreetViewModel.cs
@@ -0,0 +1,38 @@
+﻿using AdminPanel.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AdminPanel.ViewModel
+{
+    public class StreetViewModel : INotifyPropertyChanged
+    {
+        private Street selectedStreet;
+        public ObservableCollection<Street> Streets { get; set; }
+
+        public Street SelectedStreet
+        {
+            get
+            {
+                return selectedStreet;
+            }
+            set
+            {
+                selectedStreet = value;
+                OnPropertyChanged("SelectedStreet");
+            }
+        }
+
+        public StreetViewModel()
+        {
+            this.Streets = new ObservableCollection<Street>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
