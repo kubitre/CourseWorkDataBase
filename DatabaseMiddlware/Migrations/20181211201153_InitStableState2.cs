fb77fac501d4b764414e85fd7c5d686e103d7904
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-16 17:08:08 -0800

    [ADD|UPDATE] Create migrations stable

---
+++ DatabaseMiddlware/Migrations/20181211201153_InitStableState2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseMiddlware.Migrations
+{
+    public partial class InitStableState2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
