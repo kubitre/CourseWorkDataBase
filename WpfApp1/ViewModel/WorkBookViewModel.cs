a3e2b016827429c6307bb0f785c0c374009ce825
kubitre_windows_laptop <kubitre@gmail.com>
2018-12-20 21:17:25 -0800

    [ADD|UPDATE] Create viewmodel for view with model workbook and realisation INotifyPrropertyChanged

---
+++ WpfApp1/ViewModel/WorkBookViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.ViewModel
+{
+    public class WorkBookViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Models.WorkBook> WorkBooks { get; set; }
+        private Models.WorkBook selectedWorkBook;
+
+        public Models.WorkBook SelectedWorkBook
+        {
+            get
+            {
+                return selectedWorkBook;
+            }
+            set
+            {
+                selectedWorkBook = value;
+                OnPropertyChanged("SelectedWorkBook");
+            }
+        }
+
+        public WorkBookViewModel()
+        {
+            this.WorkBooks = new ObservableCollection<Models.WorkBook>();
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
